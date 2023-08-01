45deb1fa3bad9ed2c93d21a5f93c0578e53aa0f4
Srinivas Sahu <srinivas.sahu@manprax.com>
2023-08-01 10:33:10 +0530

    Added About Us page

---
+++ src/Foundation/Features/AboutUs_S/AboutUsPage_S.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using Foundation.Features.Shared;
+using Foundation.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace Foundation.Features.AboutUs_S
+{
+    [ContentType(DisplayName = "About Us Page",
+      GUID = "01237655-ef7b-47a2-a399-985f513bb4ec",
+      Description = "Used for About Us page of all sites",
+      AvailableInEditMode = true,
+      GroupName = GroupNames.Content)]
+    [ImageUrl("~/assets/icons/cms/pages/CMS-icon-page-02.png")]
+    public class AboutUsPage_S : FoundationPageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Sub content area", GroupName = SystemTabNames.Content, Order = 200)]
+        public virtual ContentArea SubContentArea { get; set; }
+    }
+}
---
+++ src/Foundation/Features/AboutUs_S/AboutUsSController.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Tracking.PageView;
+using EPiServer.Web.Mvc;
+using Foundation.Features.Home;
+using Foundation.Features.Shared;
+using System.Web.Mvc;
+
+namespace Foundation.Features.AboutUs_S
+{
+    public class AboutUsSController : PageController<AboutUsPage_S>
+    {
+        [PageViewTracking]
+        public ActionResult Index(AboutUsPage_S currentContent) => View(ContentViewModel.Create<AboutUsPage_S>(currentContent));
+    }
+}
